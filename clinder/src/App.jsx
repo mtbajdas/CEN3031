896db4c65eaa3e756dcb40d29c83da7847602915
JustinCapili <justinmarlcapili@ufl.edu>
2024-12-05 01:09:41 -0500

    Club Page Creation

--- clinder/src/App.jsx
+++ clinder/src/App.jsx
@@ -8,6 +8,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Profile from './Profile';
 import { auth } from "./firebase";
+import ClubPage from "./clubPage"
 
 function App() {
   const [count, setCount] = useState(0);
@@ -29,6 +30,7 @@ function App() {
           <Route path="/create-account" element={<CreateAccount/>}/>
           <Route path="/home" element={<Home userId={user?.uid} />}/>
           <Route path="/profile" element={<Profile/>}/>
+          <Route path="/clubs/:clubid" element={<ClubPage userId={user?.uid}/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ clinder/src/clubPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import ClubData from './clubsfile.json'
+import ToggleClub from './ToggleClub'
+
+const ClubPage = ({userId}) => {
+    const clubName = useParams();
+
+const club = ClubData.find(item => item.Organization === clubName.clubid);
+
+    return(
+        <div className="max-w-2xl mx-auto bg-white shadow-md rounded-lg p-8 mt-12">
+                <h1 className="text-4xl font-bold text-center text-slate-700 mb-4">
+                    {club.Organization}
+                </h1>
+                <h2 className="text-xl font-bold text-center text-slate-600 mb-4">
+                    {club.Categories}
+                </h2>
+            <div className="h-1 bg-slate-500 rounded-full w-24 mx-auto mb-6"></div>
+            <p className="text-lg text-gray-700 text-center leading-relaxed">
+                {club.Description}
+            </p>
+            <div className="flex justify-center mt-2">
+                <ToggleClub clubName={club.Organization} userId = {userId} />
+            </div>
+        </div>
+    )
+}
+
+export default ClubPage
