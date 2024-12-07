a0077bb2c6cedfe23d069b5b710aaf0b526303d3
Test032232 <jimmylian626@gmail.com>
2024-12-06 22:54:14 -0500

    Updated the Profile page to display a new section called Clubs Not Recommended.

--- clinder/src/ClubBox.jsx
+++ clinder/src/ClubBox.jsx
@@ -1,17 +1,15 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom/client'
+import React, { useState, useEffect } from "react"; 
+import * as ReactDOM from 'react-dom/client';
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
 import { useForm } from 'react-hook-form';
-import { useState } from 'react';
 import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate, redirect } from 'react-router-dom'; 
 import Home from './Home';
-import { useNavigate } from 'react-router-dom';
-import { redirect } from 'react-router-dom';
 import ToggleClub from './ToggleClub';
 import { auth } from './firebase';
-import { useEffect } from 'react';
+import { doc, updateDoc, arrayUnion } from "firebase/firestore";
+
 
 
 const ClubBox = (props) => {
@@ -38,3 +36,6 @@ const ClubBox = (props) => {
 }
 
 export default ClubBox;
+
+
+
--- clinder/src/Home.jsx
+++ clinder/src/Home.jsx
@@ -118,3 +118,7 @@ const Home = ({ userId }) => {
 };
 
 export default Home;
+
+
+
+
